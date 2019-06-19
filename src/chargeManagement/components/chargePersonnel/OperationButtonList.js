bedde2d0031a12920e713dae5ced315942d7cfff
xuzhipeng <569428120@qq.com>
2019-06-19 19:00:46 +0800

    aa

--- src/chargeManagement/components/chargePersonnel/OperationButtonList.js
+++ src/chargeManagement/components/chargePersonnel/OperationButtonList.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import {Button, Col, Row} from "antd";
-import ImportModal from "./ImportModal";
 
 /**
  * 操作按钮
@@ -9,15 +8,11 @@ class OperationButtonList extends Component {
 
     render() {
         return <div>
-            <ImportModal>
-                <Button>导入</Button>
-            </ImportModal>
-            <div style={{marginLeft: '68px', marginTop: '-32px'}}>
-                <Button onClick={this.props.onAddPersonnel}>新增</Button>
-                <Button onClick={this.props.onDelete}>删除</Button>
-                <Button onClick={this.props.onReduction}>减免</Button>
-                <Button onClick={this.props.onInform}>通知</Button>
-            </div>
+            <Button>导入</Button>
+            <Button onClick={this.props.onAddPersonnel}>新增</Button>
+            <Button onClick={this.props.onDelete}>删除</Button>
+            <Button onClick={this.props.onReduction}>减免</Button>
+            <Button onClick={this.props.onInform}>通知</Button>
         </div>;
     }
 }
--- src/chargeManagement/components/chargePersonnel/SearchList.js
+++ src/chargeManagement/components/chargePersonnel/SearchList.js
@@ -25,7 +25,7 @@ function ClassTreeSelect({value, onChange}) {
             <TreeNode value="1002班" title="1002班" key="random"/>
             <TreeNode value="1003班" title="1003班" key="random1"/>
         </TreeNode>
-        <TreeNode  value="二年级" title="二年级" key="0-2">
+        <TreeNode value="二年级" title="二年级" key="0-2">
             <TreeNode value="2003班" title="2003班" key="random3"/>
             <TreeNode value="2004班" title="2004班" key="random4"/>
         </TreeNode>
@@ -65,72 +65,52 @@ class SearchList extends Component {
 
     render() {
         const {getFieldDecorator} = this.props.form;
-        return <Form className="ant-advanced-search-form" style={{textAlign: 'left'}} layout="inline"
-                     onSubmit={this.onSearch}>
-            <Row span={24} style={{textAlign: 'center'}}>
-                <Col span={18}>
-                    <Row span={18}>
-                        <Col span={6}>
-                            <Form.Item label="学号">
-                                {getFieldDecorator('email', {})(
-                                    <Input style={{width: 130}}/>
-                                )}
-                            </Form.Item>
-                        </Col>
-                        <Col span={6}>
-                            <label>
-                                班级:
-                            </label>
-                            <ClassTreeSelect value={this.state.classValue} onChange={this.onChange}/>
-                        </Col>
-                        <Col span={6}>
-                            <Form.Item label="姓名">
-                                {getFieldDecorator('email', {})(
-                                    <Input style={{width: 130}}/>
-                                )}
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Row span={18}>
-                        <Col span={6}>
-                            <Form.Item label="减免">
-                                {getFieldDecorator('email', {})(
-                                    <Input style={{width: 130}}/>
-                                )}
-                            </Form.Item>
-                        </Col>
-                        <Col span={6}>
-                            <Form.Item label="状态">
-                                {getFieldDecorator('status', {})(
-                                    <Select defaultValue="lucy" allowClear={true} style={{ width: 130 }} onChange={()=>{}}>
-                                        <Option value="jack">已通知</Option>
-                                        <Option value="lucy">未通知</Option>
-                                        <Option value="Yiminghe">已缴费</Option>
-                                        <Option value="Yiminghe1">未缴费</Option>
-                                        <Option value="Yiminghe1">部分交费</Option>
-                                    </Select>
-                                )}
-                            </Form.Item>
-                        </Col>
-
-                        <Col span={6}>
-                            <Form.Item label="其他">
-                                {getFieldDecorator('email', {})(
-                                    <Input style={{width: 130}}/>
-                                )}
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                </Col>
-                <Col span={6} style={{textAlign: 'left'}}>
-                    <Button type="primary" htmlType="submit">
-                        Search
-                    </Button>
-                    <Button style={{marginLeft: 8}}>
-                        Clear
-                    </Button>
-                </Col>
-            </Row>
+        return <Form className="ant-advanced-search-form" layout="inline">
+            <Form.Item label="学号">
+                {getFieldDecorator('email', {})(
+                    <Input style={{width: 130}}/>
+                )}
+            </Form.Item>
+            <Form.Item label="姓名">
+                {getFieldDecorator('email', {})(
+                    <Input style={{width: 130}}/>
+                )}
+            </Form.Item>
+            <Form.Item label="班级">
+                <ClassTreeSelect value={this.state.classValue} onChange={this.onChange}/>
+            </Form.Item>
+            <Form.Item label="减免">
+                {getFieldDecorator('email', {})(
+                    <Input style={{width: 130}}/>
+                )}
+            </Form.Item>
+            <Form.Item label="状态">
+                {getFieldDecorator('status', {})(
+                    <Select defaultValue="lucy" allowClear={true} style={{width: 130}} onChange={() => {
+                    }}>
+                        <Option value="jack">已通知</Option>
+                        <Option value="lucy">未通知</Option>
+                        <Option value="Yiminghe">已缴费</Option>
+                        <Option value="Yiminghe1">未缴费</Option>
+                        <Option value="Yiminghe1">部分交费</Option>
+                    </Select>
+                )}
+            </Form.Item>
+            <Form.Item label="其他">
+                {getFieldDecorator('email', {})(
+                    <Input style={{width: 130}}/>
+                )}
+            </Form.Item>
+            <Form.Item>
+                <Button type="primary" htmlType="submit" onClick={this.onSearch}>
+                    Search
+                </Button>
+            </Form.Item>
+            <Form.Item>
+                <Button>
+                    Clear
+                </Button>
+            </Form.Item>
         </Form>
     }
 }
